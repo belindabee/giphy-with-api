4a08a1529e20730dc29134582377fb7992565a3c
belindabee <belindabee42@gmail.com>
2019-11-07 15:46:59 +0700

    import

--- src/components/search-result/search-result.js
+++ src/components/search-result/search-result.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import *as CustomTypes from '../../lib/custom-types'
+import SearchResults from '../search-results/search-results'
 
 export default function SearchResult ({ result }) {
-    return(<div></div>)
+    return(
+        <div>
+            { result.map({results} =><SearchResults results={results} />) }
+        </div>
+    )
 }
 
 SearchResult.PropTypes = {
